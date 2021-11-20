e9a38c3e4fe494a2c5e16e07416f8dae23d0f6d7
Ibrahim Fatai <32229780+proffatai@users.noreply.github.com>
2021-11-20 10:00:26 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,245 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is not a valid tag name?",
+    answer: "School",
+    options: [
+      "School",
+      "paragraph",
+      "Header",
+      "Table"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How do you referece a class attribute in CSS?",
+    answer: "Use of .",
+    options: [
+      "Use of #",
+      "Use of -",
+      "Use of *",
+      "Use of ."
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the followings are the two types of list in HTML?",
+    answer: "ol and ul",
+    options: [
+      "ol and li",
+      "li and ul",
+      "ol and ul",
+      " p and h1"
+    ]
+  },
+ {
+    numb: 6,
+    question: "The correct sequence of HTML tags for starting a webpage is",
+    answer: "HTML, Head, Title, Body",
+    options: [
+      "Head, Title, HTML, body",
+      "HTML, Body, Title, Head",
+      "HTML,Title, Body,Head",
+      "HTML, Head, Title, Body"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Which of the following code will increase the font size, centalize the document and change the font color of a text with class name des",
+    answer: ".des{ font-size: 20; color: red, text-align:center}",
+    options: [
+      ".des{ font-size: 20; color: red, center:True}",
+      "#des{ font-size: 20; color: red, center:True}",
+      ".des{ font-size: 20; color: red, text-align:center}",
+      "#des[font-size: 20; color: red, text-align:center]"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "<!DOCTYPE html> does what ?",
+    answer: "It tell the browser the version of the HTML",
+    options: [
+      "It tell the browser the version of the HTML",
+      "It adds styles to our website",
+      "It is added to make our website popular",
+      "It does not do anything"
+    ]
+  },
+
+
+  {
+    numb: 9,
+    question: "CSS statements must end with?",
+    answer: "semicolon (;)",
+    options: [
+      "colon (:)",
+      "semicolon (;)",
+      "full-stop (.)",
+      "Qustion mark (?)"
+    ]
+  },
+
+
+  {
+    numb: 10,
+    question: "Which of the Anchor Attribute is used to specify the language of the linked document ?",
+    answer: "None of these",
+    options: [
+      "alang",
+      "lang",
+      "hreflang",
+      "None of these"
+    ]
+  },
+
+
+  {
+    numb: 11,
+    question: "Which of the following is not part of the <table> tag"  ,
+    answer: "tc",
+    options: [
+      "td",
+      "tr",
+      "tc",
+      "None of the above"
+    ]
+  },
+
+
+  {
+    numb: 12,
+    question: "	What is the function of the <style> tag",
+    answer: " used for internal CSS",
+    options: [
+      "	Used for add images, videos and audios to our website",
+      "used for internal CSS",
+      "used for styling",
+      "used to add image"
+    ]
+  },
+
+
+  {
+    numb: 13,
+    question: "Which of the following is not a valid CSS property",
+    answer: "bgcolor",
+    options: [
+      "text-align",
+      "letter-spacing",
+      "color",
+      "bgcolor"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "Which extension will you save your CSS project with?",
+    answer: ".css",
+    options: [
+      ".html",
+      ".design",
+      ".css",
+      "None of the above"
+    ]
+  },
+
+
+  {
+    numb: 15,
+    question: "A webpage displays a picture. What tag was used to display that picture?",
+    answer: "img",
+    options: [
+      "picture",
+      "image",
+      "img",
+      "src"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "How to insert an image in HTML?",
+    answer: "<img src = image_location  />",
+    options: [
+      "using img tag and src attribute",
+      "using img tag and url attribute",
+      "using img tag and link attribute",
+      "using img tag and home attribute"
+    ]
+  }, {
+    numb: 17,
+    question: "How do you include interaction in web design?",
+    answer: "Use of Javascript",
+    options: [
+      "Use of HTML",
+      "Use of CSS",
+      "Use of Javascript",
+      "None of the above"
+    ]
+
+  },
+  {
+    numb: 18,
+    question: "Which of the following tag is used for inserting the largest heading in HTML?",
+    answer: "h1",
+    options: [
+      "h2",
+      "h7",
+      "h6",
+      "h1"
+    ]
+
+  },
+  {
+    numb: 19,
+    question: "What is the function of the <br> tag",
+    answer: "To move to a new line",
+    options: [
+      "To add table to our webpage",
+      "To move to a new line",
+      "To add color and bold our background",
+      "None of the above"
+    ]
+
+  },
+  {
+    numb: 20,
+    question: "Which of the following element is responsible for making the text bold in HTML?",
+    answer: "<b>",
+    options: [
+      "<pre>",
+      "<a>",
+      "<b>",
+      "<br>"
+    ]
+
+  },
+
+
+
+  ];
